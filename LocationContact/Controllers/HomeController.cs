75f3b88389b05d2466c965e1601b7dabc2278ab3
Nguyên Trần <Nguyn Trn@LAPTOP-NGUYENTR>
2024-11-01 00:49:40 +0700

    Add project files.

---
+++ LocationContact/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LocationContact.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Giohang()
+        {
+            return View();
+        }
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+        public ActionResult Dangki()
+        {
+            return View();
+        }
+        public ActionResult test()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult TrangChu()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult LienHe()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult TrangBlog()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ChiTietSanPham()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult TrangNike()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult TrangLacoste()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult TrangAdidas()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ LocationContact/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocationContact.MvcApplication" Language="C#" %>
